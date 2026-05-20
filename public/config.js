2c3a4adc942bb9c2880e8b0c6c3322cf39231372
cassiadix-design <cassiadix@gmail.com>
2026-05-20 12:31:55 -0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,1 +1,2 @@
-window.ANTHROPIC_API_KEY = "sk-ant-SUA-CHAVE-AQUI";
+window.ANTHROPIC_API_KEY = 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
+
